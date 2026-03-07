97dcbf7b84e771f685886ad4f989977086fd9e37
slender <trunghai1312000@gmail.com>
2026-03-07 10:40:36 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import { invoke } from "@tauri-apps/api/core";
+import "./App.css";
+
+function App() {
+  const [greetMsg, setGreetMsg] = useState("");
+  const [name, setName] = useState("");
+
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+    setGreetMsg(await invoke("greet", { name }));
+  }
+
+  return (
+    <div className="bg-red-500"> Hello World </div>
+  );
+}
+
+export default App;
